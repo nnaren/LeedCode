1fa9026e467f1e3a05ca58c340767c73dea1efa5
Ye Yuan <nnaren@163.com>
2021-06-16 20:06:37 +0800

    修改快排小问题

--- quick_sort/main.cpp
+++ quick_sort/main.cpp
@@ -7,16 +7,7 @@ int main()
     int low = 0, high = sizeof(a)/4-1;
     quickSort(a,low,high);
 
-    std::vector<int> list;
-    list.push_back(3);
-    list.push_back(6);
-    list.push_back(1);
-    list.push_back(5);
-    list.push_back(7);
-    list.push_back(8);
-    list.push_back(2);
-    list.push_back(4);
- 
+    std::vector<int> list = {3, 6, 1, 5, 7, 8, 2, 4};
     quickSortUseVector(&list, low, high);
     return 0;
 }
