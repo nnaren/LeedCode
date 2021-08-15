6ca9998d36369d0115240c2a537db18d6fe02faf
Ye Yuan <nnaren@163.com>
2021-08-15 13:51:29 +0800

    main

--- main.cpp
+++ main.cpp
@@ -1,51 +1,82 @@
 #include<iostream>
-// 可以把注释去除，看看有没有惊喜
-// void Test(int t)
+
+// // 可以把注释去除，看看有没有惊喜
+// // void Test(int t)
+// // {
+// // 	std::cout << "call Test" << std::endl;
+// // }
+// void Test( int& t)
 // {
-// 	std::cout << "call Test" << std::endl;
+//     std::cout << "call Test ref" << std::endl;
 // }
-void Test( int& t)
-{
-    std::cout << "call Test ref" << std::endl;
-}
-void Test(const int& t)
-{
-    std::cout << "call Test const ref" << std::endl;
-}
+// void Test(const int& t)
+// {
+//     std::cout << "call Test const ref" << std::endl;
+// }
 
-void Test(int&& t)
-{
-    std::cout << "call right ref" << std::endl;
-}
+// void Test(int&& t)
+// {
+//     std::cout << "call right ref" << std::endl;
+// }
 
-void Test(const int&& t)
-{
-    std::cout << "call const right ref" << std::endl;;
-}
+// void Test(const int&& t)
+// {
+//     std::cout << "call const right ref" << std::endl;;
+// }
 
-//C++标准里的转发引用
-template<class T>
-void std_forward(T&& t)
-{
-    std::cout << "test dispatch!" << std::endl;
-    Test(std::forward<T>(t));
-    // Test(t);
-}
+// //C++标准里的转发引用
+// template<class T>
+// void std_forward(T&& t)
+// {
+//     std::cout << "test dispatch!" << std::endl;
+//     Test(std::forward<T>(t));
+//     // Test(t);
+// }
 
+// int main()
+// {
+//     //注意：如果非类型精确匹配，则注释的重载可以生效
+//     //但是要注意右值的函数定义注释，否则仍然会有问题
+//     // double  a = 1.0;
+//     // Test(a);
+//     int a = 0;
+//     std_forward(a);
+
+//     const int b = 20;
+//     std_forward(b);
+
+//     std_forward(std::move(a));
+
+//     // system("pause");
+//     return 0;
+// }
+
+
+void Stand(int t)
+{
+	std::cout << "call stand function!" << std::endl;
+}
+void LeftRef(int& t)
+{
+	std::cout << "call non const LeftRef function!" << std::endl;
+}
+void LeftRef(const int& t)
+{
+	std::cout << "call const LeftRef function!" << std::endl;
+}
+void RightRef(int&& t)
+{
+	std::cout << "call non const RightRef function!" << std::endl;
+}
+void RightRef(const int&& t)
+{
+	std::cout << "call const RightRef function" << std::endl;
+}
 int main()
 {
-    //注意：如果非类型精确匹配，则注释的重载可以生效
-    //但是要注意右值的函数定义注释，否则仍然会有问题
-    // double  a = 1.0;
-    // Test(a);
-    int a = 0;
-    std_forward(a);
+    int  a = 0;
+    Stand(a);
 
-    const int b = 20;
-    std_forward(b);
+    RightRef(std::move(a));
 
-    std_forward(std::move(a));
-
-    // system("pause");
-    return 0;
 }
