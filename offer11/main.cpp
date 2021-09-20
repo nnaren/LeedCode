cd44f14378ae23651daa45158d6a78c610659b62
Yuan Ye <nnaren@163.com>
2021-09-20 19:31:45 +0800

    小修改

--- offer11/main.cpp
+++ offer11/main.cpp
@@ -17,18 +17,18 @@
 
 int main()
 {
-    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    vector<int> nums {4, 5, 6, 7, 0, 1, 2};
     cout <<  Solution::search(nums, 0) << endl;
-    vector<int> nums2 = {3, 4, 5, 6, 7, 0, 1, 2};
+    vector<int> nums2 {3, 4, 5, 6, 7, 0, 1, 2};
     cout << Solution::search(nums2, 0) << endl;
 
-    vector<int> nums3 = {3, 4, 5, 6, 7, 0, 0, 0, 0, 1, 1, 2};
+    vector<int> nums3 {3, 4, 5, 6, 7, 0, 0, 0, 0, 1, 1, 2};
     cout << Solution::search(nums3, 0) << endl;
 
-    vector<int> nums4 = {3, 1, 1};
+    vector<int> nums4 {3, 1, 1};
     cout << Solution::search(nums4, 0) << endl;
-    vector<int> nums5 = {3, 3, 1, 3};
+    vector<int> nums5 {3, 3, 1, 3};
     cout << Solution::search(nums5, 0) << endl;
-    vector<int> nums6 = {10, 10, 10, 1, 10};
+    vector<int> nums6 {10, 10, 10, 1, 10};
     cout << Solution::search(nums6, 0) << endl;
 }
