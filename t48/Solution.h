ac0e2ecb8cda9441d9a512c4bd8a05b0eff702cb
Ye Yuan <nnaren@163.com>
2021-10-21 21:47:08 +0800

    T48

--- t48/Solution.h
+++ t48/Solution.h
@@ -4,5 +4,37 @@
 
 #ifndef T48_SOLUTION_H
 #define T48_SOLUTION_H
+#include <vector>
+
+using namespace std;
+void show(vector<vector<int>>& matrix)
+{
+    for (auto v : matrix)
+    {
+        for(auto vv: v)
+        {
+            cout << vv << " " ;
+        }
+        cout << endl;
+    }
+}
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0; i<n/2; i++)
+        {
+            for(int j=i; j<n-1-i;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = temp;
+            }
+        }
+    }
+};
 
 #endif //T48_SOLUTION_H
