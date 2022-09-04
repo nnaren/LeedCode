2b8b41206fc828ab8b4c471b70d3b039d1538443
Ye Yuan <nnaren@163.com>
2022-09-04 15:22:12 +0800

    修改t946

--- t946/Solution.hpp
+++ t946/Solution.hpp
@@ -5,20 +5,41 @@ class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         stack<int> stk;
-        // stk.push();
-        for(int i = 0, j=0; j < popped.size(); ) {
+        int n = pushed.size();
+        int popIndex = 0;
+        for(int i = 0; i<n;i++){
+            stk.emplace(pushed[i]);
+            while(!stk.empty() && stk.top() == popped[popIndex]) {
+                stk.pop();
+                popIndex++;
+            }
+        }
+        return stk.empty();
+    }
+
+    bool validateStackSequences2(vector<int>& pushed, vector<int>& popped) {
+        stack<int> stk;
+        int n = pushed.size();
+        int j = 0;
+        for(int i = 0 ; i < n; ) {
             if(!stk.empty() && stk.top() == popped[j]) {
                 stk.pop();
+                cout << "pop";
                 j++;
-            } else if(pushed[i] == popped[j]) {
-                j++; i++;
             }
-             else {
-                if(i == pushed.size())
-                    break;
+            else if(pushed[i] == popped[j]) {
+                j++; i++;
+                cout << "ij";
+            }  else {
                 stk.push(pushed[i]);
                 i++;
+                cout << "push";
             }
+        }
+        while(!stk.empty() && stk.top() == popped[j]) {
+            stk.pop();
+            cout << "pop";
+            j++;
         }
         return stk.empty();
     }
