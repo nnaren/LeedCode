efda3dd1436143ec1e4ff26aad83088532d96117
red <nnaren@163.com>
2022-10-01 10:22:58 +0800

    面试题01.08.零矩阵  第一遍标记 第二遍修改  标记放在首行首列优化存储

---
+++ 面试题01.08.零矩阵/Solution.hpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        unordered_set<int> rows ;
+        unordered_set<int> cols ;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0)
+                {
+                    rows.insert(i);
+                    cols.insert(j);
+                }
+                
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] != 0 && (rows.count(i) || cols.count(j))) {
+                    matrix[i][j] = 0;
+                }
+                
+            }
+        }
+
+    }
+};
