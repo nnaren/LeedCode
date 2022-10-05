bd5d11897be571ffd5f607ad3351263d7040636a
red <nnaren@163.com>
2022-10-05 22:09:20 +0800

    t1026

--- t1026_节点与其祖先之间的最大差值/Solution.hpp
+++ t1026_节点与其祖先之间的最大差值/Solution.hpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include "../tree_struct.h"
 #include <vector>
 using namespace std;
 
--- t1026_节点与其祖先之间的最大差值/main.cpp
+++ t1026_节点与其祖先之间的最大差值/main.cpp
@@ -2,6 +2,8 @@
 #include "Solution.hpp"
 
 int main() {
+    TreeNode * root = new TreeNode(8, new TreeNode(3), new TreeNode(10));
     Solution sol;
+    cout << sol.maxAncestorDiff(root);
     return 0;
 }
