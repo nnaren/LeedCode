8195a33395414c510e3ed9ebd4edb17d727d353c
red <nnaren@163.com>
2022-10-07 19:04:29 +0800

    打印二维数组

--- show.hpp
+++ show.hpp
@@ -7,3 +7,29 @@ inline void show(T container)  {
     }
      cout << endl;
 }
+
+template<typename T>
+inline void show2D(T board) {
+    int m = board.size();
+    int n = board[0].size();
+    cout << "[";
+    for(int i = 0; i < m;i++){
+        
+        for(int j = 0; j < n; j++) {
+            if(j == 0) {
+                cout << "[" ;
+            }
+            cout << "\""<<board[i][j] << "\"" ;
+            if(j<n-1) {
+               cout <<",";
+            }
+            if(j == n-1) {
+                cout << "]";
+                if(i < m-1){
+                    cout << ",";
+                }
+            }
+        }
+    }
+    cout << "]";
+}
