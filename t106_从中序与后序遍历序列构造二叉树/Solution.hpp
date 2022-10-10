03375f4cb88562e5f0f1d2b9fbc110f26d48ef11
red <nnaren@163.com>
2022-10-10 22:12:51 +0800

    t106_从中序与后序遍历序列构造二叉树:递归/迭代+stack

---
+++ t106_从中序与后序遍历序列构造二叉树/Solution.hpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "../tree_struct.h"
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if (postorder.size() == 0) {
+            return nullptr;
+        }
+        auto root = new TreeNode(postorder[postorder.size() - 1]);
+        auto s = stack<TreeNode*>();
+        s.push(root);
+        int inorderIndex = inorder.size() - 1;
+        for (int i = int(postorder.size()) - 2; i >= 0; i--) {
+            int postorderVal = postorder[i];
+            auto node = s.top();
+            if (node->val != inorder[inorderIndex]) {
+                node->right = new TreeNode(postorderVal);
+                s.push(node->right);
+            } else {
+                while (!s.empty() && s.top()->val == inorder[inorderIndex]) {
+                    node = s.top();
+                    s.pop();
+                    inorderIndex--;
+                }
+                node->left = new TreeNode(postorderVal);
+                s.push(node->left);
+            }
+        }
+        return root;
+    }
+};
