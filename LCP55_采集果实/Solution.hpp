6026fe021b5e8bffc4b2abaff698c3820bf97ac7
red <nnaren@163.com>
2022-10-11 21:08:34 +0800

    LCP55更新

--- LCP55_采集果实/Solution.hpp
+++ LCP55_采集果实/Solution.hpp
@@ -7,8 +7,8 @@ class Solution {
 public:
     int getMinimumTime(vector<int>& time, vector<vector<int>>& fruits, int limit) {
         int sum = 0;
-        for(int i = 0; i < fruits.size(); i++){
-            sum += time[fruits[i][0]] * ((fruits[i][1] + limit-1)/ limit);
+        for(auto & fruit : fruits){
+            sum += time[fruit[0]] * ((fruit[1] + limit-1)/ limit);
         }
         return sum;
     }
